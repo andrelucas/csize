14016d4d825b97881c97d3235ff8b644380db69f
André Lucas <andre@ae-35.com>
2017-02-07 15:07:27 +0000

    %zu for size_t

--- csize.c
+++ csize.c
@@ -1,14 +1,14 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <sys/time.h>
 #include <sys/types.h>
 
-#define SIZE(type) printf("sizeof(" #type ") = %" PRIu64 "\n", sizeof(type))
+#define SIZE(type) printf("sizeof(" #type ") = %zu\n", sizeof(type))
 
 #define SIZE_STRUCT_MEM(_st, _mem)                                             \
   do {                                                                         \
     struct _st _struct = {0};                                                  \
-    printf("sizeof(struct " #_st "." #_mem ") = %" PRIu64 "\n",                \
-           sizeof(_struct._mem));                                              \
+    printf("sizeof(struct " #_st "." #_mem ") = %zu\n", sizeof(_struct._mem)); \
   } while (0)
 
 int main(int argc, char *argv[]) {
